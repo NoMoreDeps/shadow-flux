ce529f2c81975e1ca9b38d490cfa1ec656db2c48
beewix <fskorzec@gmail.com>
2017-03-31 06:03:22 +0000

    Cleanup

--- types/React/MapContainer.d.ts
+++
@@ -1,8 +0,0 @@
-import { Container } from "./Container";
-import { Dispatcher } from "../Flux";
-export declare abstract class MapContainer<T> extends Container<T> {
-    constructor(props: {
-        dispatcher: Dispatcher;
-    });
-    nextState(newStateData: any): boolean;
-}
